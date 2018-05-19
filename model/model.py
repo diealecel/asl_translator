6abd12438405245baa039ca2c4f5a9eea44373fc
Diego <diego@DN51vkn4.SUNet>
2018-05-18 23:50:42 -0700

    added compute_cost to model.py

--- model/model.py
+++ model/model.py
@@ -82,6 +82,26 @@ def forward_propagation(X, parameters):
 
 
 """
+Computes the cost
+
+Arguments:
+Z3 -- output of forward propagation (output of the last LINEAR unit), of shape (6, number of examples)
+Y -- "true" labels vector placeholder, same shape as Z3
+
+Returns:
+cost - Tensor of the cost function
+"""
+def compute_cost(Z3, Y):
+    # to fit the tensorflow requirement for tf.nn.softmax_cross_entropy_with_logits(...,...)
+    logits = tf.transpose(Z3)
+    labels = tf.transpose(Y)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+
+    return cost
+
+
+"""
 Implements a three-layer tensorflow neural network: LINEAR->RELU->LINEAR->RELU->LINEAR->SOFTMAX.
 
 Arguments:
